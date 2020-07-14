a932db8f022d8623f8851cec03bb577e7efc7e0f
DavidLee <1136437381@qq.com>
2020-07-14 17:41:38 +0800

    String练习完成

--- src/main/java/string/StringReverseExample.java
+++ src/main/java/string/StringReverseExample.java
@@ -1,10 +1,17 @@
 package string;
 
 public class StringReverseExample {
-    public static void main(String[] args){
-        String string="runoob";
-        String reverse = new StringBuffer(string).reverse().toString();
-        System.out.println("字符串反转前:"+string);
-        System.out.println("字符串反转后:"+reverse);
+//    public static void main(String[] args){
+//        String string="runoob";
+//        String reverse = new StringBuffer(string).reverse().toString();
+//        System.out.println("字符串反转前:"+string);
+//        System.out.println("字符串反转后:"+reverse);
+//    }
+
+    public static void main(String[] args) {
+        String str = "wowowowowo";
+        String reverse = new StringBuffer(str).reverse().toString();
+        System.out.println("原字符串" + str);
+        System.out.println("反转后" + reverse);
     }
 }
--- src/main/java/string/StringToUpperCaseEmp.java
+++ src/main/java/string/StringToUpperCaseEmp.java
@@ -1,10 +1,17 @@
 package string;
 
 public class StringToUpperCaseEmp {
+//    public static void main(String[] args) {
+//        String str = "string runoob";
+//        String strUpper = str.toUpperCase();
+//        System.out.println("原始字符串: " + str);
+//        System.out.println("转换为大写: " + strUpper);
+//    }
+
     public static void main(String[] args) {
-        String str = "string runoob";
+        String str = "hello rose";
         String strUpper = str.toUpperCase();
-        System.out.println("原始字符串: " + str);
-        System.out.println("转换为大写: " + strUpper);
+        System.out.println("原字符串" + str);
+        System.out.println("转换大小写后" + strUpper);
     }
 }
